7bcd69ed0e522a69e9ec5a75be992a85e47923c2
Md Jannatul Ferdhous Emon <106956927+jfemon8@users.noreply.github.com>
2023-12-24 20:23:58 +0600

    Add files via upload

---
+++ QueueUsingList.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myQueue
+{
+private:
+    list<int> l;
+
+public:
+    void push(int val)
+    {
+        l.push_back(val);
+    }
+    void pop()
+    {
+        l.pop_front();
+    }
+    int front()
+    {
+        return l.front();
+    }
+    int sz()
+    {
+        return l.size();
+    }
+    bool empty()
+    {
+        return l.empty();
+    }
+};
+
+int main()
+{
+    myQueue q;
+    while (true)
+    {
+        int x;
+        cout << endl
+             << "Option 1: Push element into Queue" << endl
+             << "Option 2: Pop element from Queue" << endl
+             << "Option 3: View front" << endl
+             << "Option 4: View size" << endl
+             << "Option 5: Exit" << endl
+             << endl
+             << "Please enter an option: ";
+        cin >> x;
+        if (x == 1)
+        {
+            int v;
+            cout << endl
+                 << "Please enter a value: ";
+            cin >> v;
+            q.push(v);
+            cout << endl
+                 << "Value inserted to the Queue successfully." << endl;
+        }
+        else if (x == 2)
+        {
+            if (!q.empty())
+            {
+                q.pop();
+                cout << endl
+                     << "Popped element from Queue." << endl;
+            }
+            else
+            {
+                cout << endl
+                     << "Queue is empty." << endl;
+            }
+        }
+        else if (x == 3)
+        {
+            if (!q.empty())
+            {
+                cout << endl
+                     << "The front value is " << q.front() << endl;
+            }
+            else
+            {
+                cout << endl
+                     << "Queue is empty." << endl;
+            }
+        }
+        else if (x == 4)
+        {
+            cout << endl
+                 << "Size of the Queue is " << q.sz() << endl;
+        }
+        else if (x == 5)
+        {
+            cout << endl
+                 << "Queue has been terminated, Thank you." << endl;
+            break;
+        }
+        else
+        {
+            cout << endl
+                 << "Invalid option." << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ QueueUsingSinglyLinkedList.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class myQueue
+{
+private:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int s = 0;
+
+public:
+    void push(int val)
+    {
+        Node *newNode = new Node(val);
+        s++;
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+    }
+    void pop()
+    {
+        s--;
+        Node *dltNode = head;
+        head = head->next;
+        if (head == NULL)
+        {
+            tail = NULL;
+        }
+        delete dltNode;
+    }
+    int front()
+    {
+        return head->val;
+    }
+    int sz()
+    {
+        return s;
+    }
+    bool empty()
+    {
+        if (s == 0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+};
+
+int main()
+{
+    myQueue q;
+    while (true)
+    {
+        int x;
+        cout << endl
+             << "Option 1: Push element into Queue" << endl
+             << "Option 2: Pop element from Queue" << endl
+             << "Option 3: View front" << endl
+             << "Option 4: View size" << endl
+             << "Option 5: Exit" << endl
+             << endl
+             << "Please enter an option: ";
+        cin >> x;
+        if (x == 1)
+        {
+            int v;
+            cout << endl
+                 << "Please enter a value: ";
+            cin >> v;
+            q.push(v);
+            cout << endl
+                 << "Value inserted to the Queue successfully." << endl;
+        }
+        else if (x == 2)
+        {
+            if (!q.empty())
+            {
+                q.pop();
+                cout << endl
+                     << "Popped element from Queue." << endl;
+            }
+            else
+            {
+                cout << endl
+                     << "Queue is empty." << endl;
+            }
+        }
+        else if (x == 3)
+        {
+            if (!q.empty())
+            {
+                cout << endl
+                     << "The front value is " << q.front() << endl;
+            }
+            else
+            {
+                cout << endl
+                     << "Queue is empty." << endl;
+            }
+        }
+        else if (x == 4)
+        {
+            cout << endl
+                 << "Size of the Queue is " << q.sz() << endl;
+        }
+        else if (x == 5)
+        {
+            cout << endl
+                 << "Queue has been terminated, Thank you." << endl;
+            break;
+        }
+        else
+        {
+            cout << endl
+                 << "Invalid option." << endl;
+        }
+    }
+
+    return 0;
+}
